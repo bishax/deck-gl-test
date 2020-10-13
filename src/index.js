b175b4a574e10f37fc879a6f7bb96a0c1e35c053
bishax <alex.bishop@nesta.org.uk>
2020-10-13 13:20:28 +0100

    Added deck.gl arc example

--- src/index.js
+++ src/index.js
@@ -4,12 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Source data GeoJSON
+const DATA_URL =
+  'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/arc/counties.json'; // eslint-disable-line
+
+const container = document.getElementById('root');
+ReactDOM.render(<App />, container);
+
+fetch(DATA_URL)
+  .then(response => response.json())
+  .then(({features}) => {
+    ReactDOM.render(<App data={features} />, container);
+  });
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
